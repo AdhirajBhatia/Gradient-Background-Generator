ad34665b840fc0a1e8d9b799f27d07155dd3700e
Adhiraj Bhatia <adhirajb1109@gmail.com>
2021-04-05 10:25:09 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+var css = document.querySelector("code");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("grad");
+
+function setGradient() {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+
+  css.textContent = body.style.background + ";";
+}
+
+setGradient();
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
